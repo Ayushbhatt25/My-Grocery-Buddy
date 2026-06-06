9ce1154332ddd9e5acfb49502fb0d1220bfbe8c4
unknown <sgesportsblizzy@gmail.com>
2026-06-06 11:59:58 +0530

    Update origins

--- server/server.js
+++ server/server.js
@@ -20,8 +20,8 @@ connectCloudinary();
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://greencart-sand.vercel.app',
-    'https://my-grocery-buddy.vercel.app'
+    'https://my-grocery-buddy.vercel.app',
+    'https://my-grocery-buddy-l0cwln1dr-ayushbhatt25s-projects.vercel.app/'
 ];
 if (process.env.FRONTEND_URL) {
     allowedOrigins.push(process.env.FRONTEND_URL.replace(/\/$/, ""));
@@ -34,9 +34,9 @@ app.use(cors({
     origin: function (origin, callback) {
         if (!origin) return callback(null, true);
         const cleanedOrigin = origin.replace(/\/$/, "");
-        const isAllowed = allowedOrigins.includes(cleanedOrigin) || 
-                          (cleanedOrigin.endsWith('.vercel.app') && cleanedOrigin.includes('ayushbhatt25s-projects'));
-        
+        const isAllowed = allowedOrigins.includes(cleanedOrigin) ||
+            (cleanedOrigin.endsWith('.vercel.app') && cleanedOrigin.includes('ayushbhatt25s-projects'));
+
         if (isAllowed) {
             callback(null, true);
         } else {
